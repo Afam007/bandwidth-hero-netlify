4d15cf49fc94032d2c38594b68eac7b35034b5c2
70SO <69805385+hos1818@users.noreply.github.com>
2024-09-27 06:52:14 +0300

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -10,14 +10,12 @@ const proxy = require('../src/proxy')
 const app = express()
 const router = express.Router()
 
-// HTTP request logging
-app.use(morgan('combined'))
-
 router.enable('trust proxy')
 router.get('/', authenticate, params, proxy)
 router.get('/favicon.ico', (req, res) => res.status(204).end())
 
-app.use("/api/", router);
+app.use("/.netlify/functions/server", router);
 
 // Export the app wrapped in serverless
+module.exports = app;
 module.exports.handler = serverless(app);
